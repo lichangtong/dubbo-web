e684439ac47af0eaad7b8169aee0e160e3072ed7
lichangtong <710004168@qq.com>
2017-08-28 20:46:40 +0800

    配置提交

--- ivan-dubbo-server/src/main/java/com/ivan/service/impl/ServerMain.java
+++ ivan-dubbo-server/src/main/java/com/ivan/service/impl/ServerMain.java
@@ -1,5 +1,6 @@
 package com.ivan.service.impl;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -9,12 +10,15 @@ import java.io.IOException;
  * @create 2017-08-21 19:49
  **/
 public class ServerMain {
-    public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[]{"spring-registry.xml"});
-        context.start();
-
-        System.out.println("按任意键退出！！！");
-        System.in.read(); // 按任意键退出
-    }
+//    public static void main(String[] args) throws IOException {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                new String[]{"spring-registry.xml"});
+//        context.start();
+//
+//        System.out.println("按任意键退出！！！");
+//        System.in.read(); // 按任意键退出
+//
+//
+//
+//    }
 }
